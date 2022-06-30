fc02c6b070e833453f0e7375c06a85c9e0134d51
root <Daniel Teklu>
2022-06-30 14:24:45 -0700

    addign 6 again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,8 @@
 
 /**
  *
- *  * main - prints the size of various types
- *
- *   * Return: 0 if exited properly, non-zero otherwise
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  *
  */
 
