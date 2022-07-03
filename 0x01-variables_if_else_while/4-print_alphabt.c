8adf5d72c8a2fd474c59f1b6f0602c07134071ed
root <Daniel Teklu>
2022-07-03 12:07:25 -0700

    errors fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	char ch = 'a';
-	
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
@@ -17,7 +16,7 @@ int main(void)
 			putchar(ch);
 		}
 		ch++;
-	} 
+	}
 	putchar('\n');
 	return (0);
 }
